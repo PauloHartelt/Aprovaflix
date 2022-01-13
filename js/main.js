b09847483fcd86b1c391b55233630c8a6aadc658
Paulo <paulohartelt@hotmail.com>
2022-01-13 09:42:27 -0300

    Aprovaflix

---
+++ js/main.js
@@ -0,0 +1,37 @@
+let line = gets().split(" A ; B ");
+let A = parseInt(line[0]);
+let B = parseInt(line[1]);
+let total = A + B;
+console.log("X = " + total);
+
+
+
+/*function clicou(){document.getElementById("agradecimento").innerHTML = "Obrigado por clicar";}
+
+function redirecionar() {window.open("https://www.youtube.com/");}
+
+function trocar() {document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse";}
+
+function voltar() {document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+    
+}
+
+
+var d = new Date();
+alert(d.getMinutes());
+var count;
+for (count = 0; count<=5; count++){alert(count)};
+var idade = prompt("Qual a sua idade");
+if (idade >= 18) {alert("Maior de idade")}else{alert("Menor de idade");};
+var lista = ["maçã","pêra","laranja"];
+    console.log(lista);
+    var nome = "Rafael Galleani"; 
+    var idade = 29; 
+    var idade2 = 10;
+    var frase = "Japão é o melhor time do mundo";
+
+    alert (nome + " tem " + idade + " anos");
+    alert (idade + idade2) 
+    console.log(frase.replace("Japão", "Brasil")); */
+
+
